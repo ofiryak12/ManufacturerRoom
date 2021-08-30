d942defec030d9ae3415028423f3e071e7f0561a
ofir <ofiryak12@gmail.com>
2021-08-30 09:28:08 +0300

    update

--- manufactureview/manufactureview/urls.py
+++ manufactureview/manufactureview/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('pt.urls')),
+    # path('',include('pt.urls')),
+    path('',include('frontend.urls')),
 ]
